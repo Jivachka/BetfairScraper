fb5ec596f4cb0f463fdeaa5aa6adba8e1f6fa710
AleksanderYa <250000ye@gmail.com>
2023-11-24 10:59:54 +0200

    add some import

--- class_work.py
+++ class_work.py
@@ -3,6 +3,10 @@ import random
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 HENDLESS=False
 
